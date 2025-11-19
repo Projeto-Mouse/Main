e94d6d3699914fb4c8c9ad3c5dfb4f7283419306
Mapnbp <matheusaraujopinh@gmail.com>
2025-11-19 01:30:51 -0300

    chore: corrige erros de lint e deleta arquivos de lógica duplicada

--- Menu Principal/Menu de Opcoes/MenuDeOpcoes.gd
+++ Menu Principal/Menu de Opcoes/MenuDeOpcoes.gd
@@ -1,15 +1,14 @@
 class_name MenuDeOpcoes
 extends Control
 
-@onready var sair_do_menu_Button = $MarginContainer/VBoxContainer/sair_do_menu_Button as Button
+@onready var sair_do_menu_button = $MarginContainer/VBoxContainer/sair_do_menu_Button as Button
 
-# Auxilia a sair do menu de opcoes para voltar ao menu principal
 signal sair_das_opcoes
 
 func _ready():
-	sair_do_menu_Button.button_down.connect(on_sair_pressed)
+	sair_do_menu_button.button_down.connect(on_sair_pressed)
 	set_process(false)
-	
+
 func on_sair_pressed() -> void:
 	get_tree().paused = false
 	Engine.time_scale = 1
