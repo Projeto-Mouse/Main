bbdba3dffec588f5c90b8241d3a7617eb25a4587
Matheus Araujo Pinheiro <165668357+Mapnbp@users.noreply.github.com>
2025-11-19 01:33:43 -0300

    chore: corrige erros de lint e deleta arquivos de lógica duplicada (#39)

--- Menu Principal/VideoMenu.gd
+++ Menu Principal/VideoMenu.gd
@@ -1,12 +1,9 @@
 extends VideoStreamPlayer
 
-# Tem que implementar a funcao de ambos quando 
-# configurarmos o menu efeito do menu dependendo da honra, por hora, so gerei o script
+# TODO:configurar o menu efeito do menu dependendo da honra
 
-# Chamado quando o nó entra na árvore de cena pela primeira vez.
 func _ready() -> void:
-	pass 
+	pass
 
-# Chamado de cada quadro. 'delta' é o tempo decorrido desde o quadro anterior.
 func _process(delta: float) -> void:
 	pass
