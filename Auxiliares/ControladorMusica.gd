dbc05c825694d74647abe2c25b38afac48e2bddb
Pedro Henrique Gonçalves de Paula <69373766+pedrohgdp@users.noreply.github.com>
2025-11-18 23:57:15 -0300

    Pmdt 29/pmdt 35/criar pasta auxiliares retarorar arquivos auxiliares (#29)
    * Add multilingual setup tutorials for Project Mouse
    
    Added detailed setup tutorials in Portuguese, English, and Spanish for Project Mouse, including prerequisites, repository configuration, and workflow guidelines.
    
    * Correct code block syntax in README.md
    
    Fixed code block formatting in README.md for better readability.
    
    * Create pull request template for contributions
    
    Add a comprehensive pull request template for contributors.
    
    * Delete .github/PULL_REQUEST_TEMPLATE/PULL_REQUEST_TEMPLATE.md
    
    * Create pull request template for contributions
    
    Add a pull request template with detailed sections for ticket information, implementation, testing, and additional notes.
    
    * Fix headings in support and funding sections (#22)
    
    * PMDT-16/PMDT-18/PMDT-22 configurei as pastas dos personagens, refatorei e melhore o codigo, verifiquei se esta padrao. Fiz tudo numa mesma branch, pois uma tarefa encadeou a outra.
    
    * FIT - Criado pasta de auxiliares
    
    * FIX - Refatado codigo de ControladorMusica
    
    * FIT - Criado uma pasta Sons onde fica as musicas e os efeitos sonoros
    
    * FIX - Ajeitado bugs na forno
    
    ---------
    
    Co-authored-by: Matheus Araujo Pinheiro <165668357+Mapnbp@users.noreply.github.com>

--- Auxiliares/ControladorMusica.gd
+++ Auxiliares/ControladorMusica.gd
@@ -1,18 +1,26 @@
 class_name ControladorMusica
 extends Node
 
+# NAO SEI SE TWEEN E MUITO BOA EM BIG O ( O(1) DE ACORDO COM GPT ) 
+# E NAO SEI SE O CODIGO ESTA BOM SUFICIENTE - TEMOS QUE VER
+
+
 @export var duracao_fade : float = 0.5
 @export var volume_alvo_db : float = 0.0
 
+# Repodutor faz tudo na questão de controlar o volume, tocar uma musica entre outros
 var reprodutor = AudioStreamPlayer
 
+# Ready e a funcao que comeca quando iniciamos o jogo
 func _ready() -> void:
+	# Ela instancia reprodutor e seta o volume dele
 	reprodutor = AudioStreamPlayer.new()
 	add_child(reprodutor)
 	reprodutor.volume_db = volume_alvo_db
 
 # Funcao de tocar a musica
 func tocar_musica(musica_audio: AudioStream, repetir: bool = false) -> void:
+	#Paro a musica sendo tocada se repetir for verdade todo a musica com loop
 	reprodutor.stop() 
 	reprodutor.stream = musica_audio
 	if repetir:
@@ -23,6 +31,8 @@ func tocar_musica(musica_audio: AudioStream, repetir: bool = false) -> void:
 # PENSAR SE VAI USAR FADE_OUT E FADE_IN
 func trocar_musica(musica_audio : AudioStream) -> void:
 	if reprodutor.playing:
+		# await e do proprio godot e diz que a proxima coisa só ocorre quando esse evendo de fade_out acabar
+		# ou seja ele pausa essa funcao e começa a fade_out quando acabar fade_out() volta para ca
 		await fade_out()
 	reprodutor.stream = musica_audio
 	reprodutor.play()
@@ -39,6 +49,7 @@ func parar_musica() -> void:
 func fade_out() -> void:
 	var tween = create_tween()
 	tween.tween_property(reprodutor, "volume_db", -80.0, duracao_fade)
+	# Avisa que o tween acabou
 	await tween.finished
 
 
