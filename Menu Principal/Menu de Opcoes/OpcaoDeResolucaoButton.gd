62556737847d20c6af9bd8718b0810d0e4c38baa
Matheus Araujo Pinheiro <165668357+Mapnbp@users.noreply.github.com>
2025-11-20 23:18:51 -0300

    chore: corrige erros de lint e deleta arquivos de lógica duplicada (#50)

--- Menu Principal/Menu de Opcoes/OpcaoDeResolucaoButton.gd
+++ Menu Principal/Menu de Opcoes/OpcaoDeResolucaoButton.gd
@@ -2,25 +2,22 @@ extends Control
 
 @onready var option_button: OptionButton = $HBoxContainer/OptionButton
 
-# Tabela hash para as opções de resoulções
-# Eh muito facil de adicionar mais opções de resolução depois tratando isso como uma tabela hash
-const DIC_DE_RESOLUCOES : Dictionary = {
+# Hash para resoulções
+const dic_de_resolucoes : Dictionary = {
 	"1152 x 648" : Vector2i(1152, 638),
 	"1280 x 720" : Vector2i(1280, 720),
 	"1920 x 1080" : Vector2i(1920, 1080),
 	"2440 x 1920" : Vector2i(2440, 1920)
-} 
+}
 
 func _ready():
 	add_opcao_resolucao()
 	option_button.item_selected.connect(on_resolucao_selecionada)
 	
-# Adciona as opções no selecionador das opções graficas do menu
 func add_opcao_resolucao() -> void:
-	for resolucao_text in DIC_DE_RESOLUCOES:
+	for resolucao_text in dic_de_resolucoes:
 		option_button.add_item(resolucao_text)
 		
-# Altera a resolução da imagem baseado no que foi selecionado
-# Usei uma gamb que ja puxa direto do vetor2i as informacoes
 func on_resolucao_selecionada(index: int) -> void:
-	DisplayServer.window_set_size(DIC_DE_RESOLUCOES.values()[index])
+	var nova_resolucao = dic_de_resolucoes.values()[index]
+	DisplayServer.window_set_size(nova_resolucao)
