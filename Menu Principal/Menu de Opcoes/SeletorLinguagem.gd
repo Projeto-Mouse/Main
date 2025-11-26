085df6cf304c75ea4b1f44e08fdaa798841f1ec8
pedrohgdp <pedro.henrique.gp@outlook.com>
2025-11-26 19:35:56 +0000

    FEAT: Criado pasta traducao com traduces / Criado singleton para setar linguagem default e funcao de trocar linguagem / Criado botoes para trocar linguagem

---
+++ Menu Principal/Menu de Opcoes/SeletorLinguagem.gd
@@ -0,0 +1,14 @@
+class_name SeletorLinguagem
+extends MarginContainer
+
+
+func _on_portugues_pressed() -> void:
+	ControladorTraducao.set_traducao("pt_BR")
+
+
+func _on_ingles_pressed() -> void:
+	ControladorTraducao.set_traducao("en_US")
+
+
+func _on_espanhol_pressed() -> void:
+	ControladorTraducao.set_traducao("es_ES")
