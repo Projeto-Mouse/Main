478111d61d8936fef7ba5c668791f5012839c22b
pedrohgdp <pedro.henrique.gp@outlook.com>
2025-11-26 20:10:04 +0000

    FIX: EXCLUSAO ARQUIVOS DESNECESSARIOS

--- Auxiliares/ControladorLinguagem.gd
+++
@@ -1,9 +0,0 @@
-class_name ControladorLinguagem
-extends Node
-
-func _ready() -> void:
-	#Idioma padrao e ptBR
-	TranslationServer.set_locale("pt_BR")
-
-func set_linguagem(codigo_do_local) -> void:
-	TranslationServer.set_locale(codigo_do_local)
