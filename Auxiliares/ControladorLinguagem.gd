5d2fcfb493dcb25f2e80a0435b58594b94fe81fd
Pedro Henrique Gonçalves de Paula <69373766+pedrohgdp@users.noreply.github.com>
2025-11-26 08:35:25 -0300

    Pmdt 37/traducao para ingles espanhol adicionado ao forno
    * Pmdt 29/pmdt 35/criar pasta auxiliares retarorar arquivos auxiliares (#49)
    
    * FIT - Criado pasta Auxiliar e pasta Sons, Comentarios desnecessarios tirados do codigo ControladorMusica
    
    * Removido Linhas extras e comentarios desnecessarios
    
    ---------
    
    Co-authored-by: pedrohgdp <pedro.henrique.gp@outlook.com>
    Co-authored-by: Pedro Henrique Gonçalves de Paula <69373766+pedrohgdp@users.noreply.github.com>
    
    * chore: corrige erros de lint e deleta arquivos de lógica duplicada (#50)
    
    * chore: corrige erros de lint e remove comentários desnecessarios (#51)
    
    * Adicionando novamente a pasta Sprites e realocando a pasta Imagens de… (#52)
    
    Adicionando novamente a pasta Sprites e realocando a pasta Imagens de Backgorund e ImagensVidas
    
    Co-authored-by: Wesley H. de Brito - WesleyRTw <wesleyheringer24@gmail.com>
    
    * Feat - Criado sistema de traducao para o jogo, com traducao para ingles e espanhol
    
    * Delete .vscode/settings.json
    
    ---------
    
    Co-authored-by: Matheus Araujo Pinheiro <165668357+Mapnbp@users.noreply.github.com>
    Co-authored-by: Wesley H. de Brito - WesleyRTw <wesleyheringer24@gmail.com>

--- Auxiliares/ControladorLinguagem.gd
+++ Auxiliares/ControladorLinguagem.gd
@@ -1,9 +1,9 @@
 class_name ControladorLinguagem
 extends Node
-# Classe para carregar o idioma global e ser chamada quando mudar o idioma
 
 func _ready() -> void:
+	#Idioma padrao e ptBR
 	TranslationServer.set_locale("pt_BR")
-	
+
 func set_linguagem(codigo_do_local) -> void:
 	TranslationServer.set_locale(codigo_do_local)
--- Menu Principal/Menu de Opcoes/SelecaoLinguagem.gd
+++ Menu Principal/Menu de Opcoes/SelecaoLinguagem.gd
@@ -2,10 +2,10 @@ class_name SelecaoLinguagem
 extends MarginContainer
 
 func _on_portugues_pressed() -> void:
-	ControladorLingua.set_linguagem("pt_BR")
-
-func _on_inglês_pressed() -> void:
-	ControladorLingua.set_linguagem("en_US")
+	ControladorTraducao.set_linguagem("pt_BR")
+  
+func _on_ingles_pressed() -> void:
+	ControladorTraducao.set_linguagem("en_US")
 
 func _on_espanhol_pressed() -> void:
-	ControladorLingua.set_linguagem("es_ES")
+	ControladorTraducao.set_linguagem("es_ES")
