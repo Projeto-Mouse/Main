9e77cf152e8d26f6677cd5ae31937de6d0b54cb4
pedrohgdp <pedro.henrique.gp@outlook.com>
2025-11-26 20:59:07 -0300

    FIX: Cagada que fiz

--- Auxiliares/ControladorLinguagem.gd
+++
@@ -1,9 +0,0 @@
-class_name ControladorLinguagem
-extends Node
-# Classe para carregar o idioma global e ser chamada quando mudar o idioma
-
-func _ready() -> void:
-	TranslationServer.set_locale("pt_BR")
-	
-func set_linguagem(codigo_do_local) -> void:
-	TranslationServer.set_locale(codigo_do_local)
