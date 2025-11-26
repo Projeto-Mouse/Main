d75a38668e97d6ac14c0e9850dbb81fb3a342066
pedrohgdp <pedro.henrique.gp@outlook.com>
2025-11-26 07:35:54 -0300

    Feat - Criado sistema de traducao para o jogo, com traducao para ingles e espanhol

---
+++ Auxiliares/ControladorLinguagem.gd
@@ -0,0 +1,9 @@
+class_name ControladorLinguagem
+extends Node
+
+func _ready() -> void:
+	#Idioma padrão é português do Brasil
+	TranslationServer.set_locale("pt_BR")
+
+func set_linguagem(codigo_do_local) -> void:
+	TranslationServer.set_locale(codigo_do_local)
