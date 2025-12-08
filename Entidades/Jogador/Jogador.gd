cb6b37d8c6fcff4291e5276c935997568b2630d9
Pedro Henrique Gonçalves de Paula <69373766+pedrohgdp@users.noreply.github.com>
2025-12-08 14:11:19 -0300

    Correcao nome de variavel para os padroes estabelecidos no projeto PT2

--- Entidades/Jogador/Jogador.gd
+++ Entidades/Jogador/Jogador.gd
@@ -9,8 +9,8 @@ extends Personagem
 @onready var collisionShape: CollisionShape3D = $CollisionShape3D
 @onready var meshInstance: MeshInstance3D = $MeshInstance3D
 
-var movimento_x : float
-var movimento_y : float
+var movimentoX : float
+var movimentoY : float
 
 # Função de movimentação básica
 func _physics_process(delta):
@@ -34,26 +34,26 @@ func _physics_process(delta):
 	if Input.is_action_pressed("Rastejar"):
 		collisionShape.rotation_degrees.x = 90
 		meshInstance.rotation_degrees.x = 90
-		movimento_x = direcao.x * (velocidade - 2.5)
+		movimentoX = direcao.x * (velocidade - 2.5)
 	elif Input.is_action_pressed("Devagar"):
 		collisionShape.rotation_degrees.x = 0
 		meshInstance.rotation_degrees.x = 0
-		movimento_x = direcao.x * (velocidade - 2.0)
+		movimentoX = direcao.x * (velocidade - 2.0)
 	else:
 		collisionShape.rotation_degrees.x = 0
 		meshInstance.rotation_degrees.x = 0
-		movimento_x = direcao.x * velocidade
+		movimentoX = direcao.x * velocidade
 	
 	# Pular
 	if not is_on_floor():
-		movimento_y += gravidade * delta
+		movimentoY += gravidade * delta
 	else: 
 		movimento_y = 0
 		if Input.is_action_pressed("Pular"):
-			movimento_y = forca_pulo
+			movimentoY = forca_pulo
 	
 	
-	movimentacao(movimento_x, movimento_y)
+	movimentacao(movimentoX, movimentoY)
 	
 	# Teste temporario para computar dano
 	if Input.is_action_just_pressed("Dano"):
