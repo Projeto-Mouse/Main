9f5b8575c83e68495b0760d43d3675726242b539
Pedro Henrique Gonçalves de Paula <69373766+pedrohgdp@users.noreply.github.com>
2025-12-08 14:27:32 -0300

    Agora sim, configurado para a convensao certa. (CAGUEI TUDO NAS OUTRAS)

--- Entidades/Jogador/Jogador.gd
+++ Entidades/Jogador/Jogador.gd
@@ -2,15 +2,15 @@ class_name Jogador
 extends Personagem
 
 @onready var camera: Camera3D = $pivo_Camera/Camera
-@onready var coracoesVida: Control = $"../CanvasLayer/BarraVida"
+@onready var coracoes_vida: Control = $"../CanvasLayer/BarraVida"
 
 # Essas variaveis sao testes apenas para rotarcionamos o boneco para testar
 # A logica, depois serao adicionados os sprites de em pe e rastejando.
-@onready var collisionShape: CollisionShape3D = $CollisionShape3D
-@onready var meshInstance: MeshInstance3D = $MeshInstance3D
+@onready var collision_shape: CollisionShape3D = $CollisionShape3D
+@onready var mesh_instance: MeshInstance3D = $MeshInstance3D
 
-var movimentoX: float
-var movimentoY: float
+var movimento_x: float
+var movimento_y: float
 
 # Função de movimentação básica
 func _physics_process(delta):
@@ -34,26 +34,26 @@ func _physics_process(delta):
 	if Input.is_action_pressed("Rastejar"):
 		collisionShape.rotation_degrees.x = 90
 		meshInstance.rotation_degrees.x = 90
-		movimentoX = direcao.x * (velocidade - 2.5)
+		movimento_x = direcao.x * (velocidade - 2.5)
 	elif Input.is_action_pressed("Devagar"):
 		collisionShape.rotation_degrees.x = 0
 		meshInstance.rotation_degrees.x = 0
-		movimentoX = direcao.x * (velocidade - 2.0)
+		movimento_x = direcao.x * (velocidade - 2.0)
 	else:
 		collisionShape.rotation_degrees.x = 0
 		meshInstance.rotation_degrees.x = 0
-		movimentoX = direcao.x * velocidade
+		movimento_x = direcao.x * velocidade
 	
 	# Pular
 	if not is_on_floor():
-		movimentoY += gravidade * delta
+		movimento_y += gravidade * delta
 	else: 
-		movimentoY = 0
+		movimento_y = 0
 		if Input.is_action_pressed("Pular"):
-			movimentoY = forca_pulo
+			movimento_y = forca_pulo
 	
 	
-	movimentacao(movimentoX, movimentoY)
+	movimentacao(movimento_x, movimento_y)
 	
 	# Teste temporario para computar dano
 	if Input.is_action_just_pressed("Dano"):
