01adc80de88223ef3d785efabfca44b101f78815
pedrohgdp <pedro.henrique.gp@outlook.com>
2025-12-08 13:48:30 -0300

    Adicionado ato de rastejar mudando a forma do boneco para teste, variaveis e modo de rastejo provisorio, sera trocado para troca de sprites

--- Entidades/Jogador/Jogador.gd
+++ Entidades/Jogador/Jogador.gd
@@ -4,6 +4,11 @@ extends Personagem
 @onready var camera: Camera3D = $pivo_Camera/Camera
 @onready var coracoes_vida: Control = $"../CanvasLayer/BarraVida"
 
+# Essas variaveis sao testes apenas para rotarcionamos o boneco para testar
+# A logica, depois serao adicionados os sprites de em pe e rastejando.
+@onready var collisionShape: CollisionShape3D = $CollisionShape3D
+@onready var meshInstance: MeshInstance3D = $MeshInstance3D
+
 var movimento_x : float
 var movimento_y : float
 
@@ -11,7 +16,7 @@ var movimento_y : float
 func _physics_process(delta):
 	var direcao = Vector3.ZERO
 	
-	# Movimentação do corpo pelo cenário
+	# Movimentacao esquerda direita cima baixo
 	if Input.is_action_pressed("Direita"):
 		direcao.x += 1
 	if Input.is_action_pressed("Esquerda"):
@@ -20,23 +25,34 @@ func _physics_process(delta):
 		direcao.y -= 1
 	if Input.is_action_pressed("Baixo"):
 		direcao.y += 1
-	if Input.is_action_pressed("Devagar"):
-		movimento_x = direcao.x * (velocidade - 2.0)
-	else:
-		movimento_x = direcao.x * velocidade
-	
-	if not is_on_floor():
-		movimento_y += gravidade * delta
-	else: 
-		# Zera a queda para evitar acúmulo
-		movimento_y = 0
-		# Apenas quando tem contato com o chão
-		if Input.is_action_pressed("Pular"):
-			movimento_y = forca_pulo
 		
 	direcao = direcao.normalized()
 	
-	# Chama o método para mover, presente na classe Personagem
+	# Rastejar / andar devagar
+	# Provisorio o que ele muda quando rasteja
+	# No caso as rotacoes.
+	if Input.is_action_pressed("Rastejar"):
+		collisionShape.rotation_degrees.x = 90
+		meshInstance.rotation_degrees.x = 90
+		movimento_x = direcao.x * (velocidade - 2.5)
+	elif Input.is_action_pressed("Devagar"):
+		collisionShape.rotation_degrees.x = 0
+		meshInstance.rotation_degrees.x = 0
+		movimento_x = direcao.x * (velocidade - 2.0)
+	else:
+		collisionShape.rotation_degrees.x = 0
+		meshInstance.rotation_degrees.x = 0
+		movimento_x = direcao.x * velocidade
+	
+	# Pular
+	if not is_on_floor():
+		movimento_y += gravidade * delta
+	else: 
+		movimento_y = 0
+		if Input.is_action_pressed("Pular"):
+			movimento_y = forca_pulo
+	
+	
 	movimentacao(movimento_x, movimento_y)
 	
 	# Teste temporario para computar dano
