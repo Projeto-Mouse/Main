b43932c2bcbb5b65bb8bc83de2bf82c0724e2f4f
pedrohgdp <pedro.henrique.gp@outlook.com>
2025-12-08 21:52:36 -0300

    TIRANDO LOGICA NO IF DO DEVAGAR POR QUE ERA DESNECESSARIO

--- Entidades/Jogador/Jogador.gd
+++ Entidades/Jogador/Jogador.gd
@@ -14,46 +14,44 @@ var movimento_y : float
 
 # Função de movimentação básica
 func _physics_process(delta):
-    var direcao = Vector3.ZERO
+	var direcao = Vector3.ZERO
 
-    # Movimentacao esquerda direita cima baixo
-    if Input.is_action_pressed("Direita"):
-        direcao.x += 1
-    if Input.is_action_pressed("Esquerda"):
-        direcao.x -= 1
-    if Input.is_action_pressed("Cima"):
-        direcao.y -= 1
-    if Input.is_action_pressed("Baixo"):
-        direcao.y += 1
+	# Movimentacao esquerda direita cima baixo
+	if Input.is_action_pressed("Direita"):
+		direcao.x += 1
+	if Input.is_action_pressed("Esquerda"):
+		direcao.x -= 1
+	if Input.is_action_pressed("Cima"):
+		direcao.y -= 1
+	if Input.is_action_pressed("Baixo"):
+		direcao.y += 1
 
-    direcao = direcao.normalized()
+	direcao = direcao.normalized()
 
-    # Rastejar / andar devagar
-    # Provisorio o que ele muda quando rasteja
-    # No caso as rotacoes.
-    if Input.is_action_pressed("Rastejar"):
-        collision_shape.rotation_degrees.x = 90
-        mesh_instance.rotation_degrees.x = 90
-        movimento_x = direcao.x * (velocidade - 2.5)
-    elif Input.is_action_pressed("Devagar"):
-        collision_shape.rotation_degrees.x = 0
-        mesh_instance.rotation_degrees.x = 0
-        movimento_x = direcao.x * (velocidade - 2.0)
-    else:
-        collision_shape.rotation_degrees.x = 0
-        mesh_instance.rotation_degrees.x = 0
-        movimento_x = direcao.x * velocidade
+	# Rastejar / andar devagar
+	# Provisorio o que ele muda quando rasteja
+	# No caso as rotacoes.
+	if Input.is_action_pressed("Rastejar"):
+		collision_shape.rotation_degrees.x = 90
+		mesh_instance.rotation_degrees.x = 90
+		movimento_x = direcao.x * (velocidade - 2.5)
+	elif Input.is_action_pressed("Devagar"):
+		movimento_x = direcao.x * (velocidade - 2.0)
+	else:
+		collision_shape.rotation_degrees.x = 0
+		mesh_instance.rotation_degrees.x = 0
+		movimento_x = direcao.x * velocidade
 
-    # Pular
-    if not is_on_floor():
-        movimento_y += gravidade * delta
-    else:
-        movimento_y = 0
-        if Input.is_action_pressed("Pular"):
-            movimento_y = forca_pulo
+	# Pular
+	if not is_on_floor():
+		movimento_y += gravidade * delta
+	else:
+		movimento_y = 0
+		if Input.is_action_pressed("Pular"):
+			movimento_y = forca_pulo
 
-    movimentacao(movimento_x, movimento_y)
+	movimentacao(movimento_x, movimento_y)
 
-    # Teste temporario para computar dano
-    if Input.is_action_just_pressed("Dano"):
-        computar_dano(dano)
+	# Teste temporario para computar dano
+	if Input.is_action_just_pressed("Dano"):
+		computar_dano(dano)
