2fb20c0993bfff978dc728ec994efaf59738437e
Pedro Henrique Gonçalves de Paula <69373766+pedrohgdp@users.noreply.github.com>
2025-12-10 22:41:31 -0300

    Pmdt 44 adicao capacidade escalada (#86)
    * ADICIONADO CIPO NA CENA
    
    * ADICAO CODIGO QUEBRADO DE ESCALADA PARA SER CONCERTADO EM CASA
    
    * Acertado bug de nao andar, tirado cipo de cena para nao dar conflito com cena nova

--- Entidades/Jogador/Jogador.gd
+++ Entidades/Jogador/Jogador.gd
@@ -1,19 +1,47 @@
 class_name Jogador
-extends Personagem
+extends Entidade
 
 @onready var camera: Camera3D = $pivo_Camera/Camera
 @onready var coracoes_vida: Control = $"../CanvasLayer/BarraVida"
 
-# Essas variaveis sao testes apenas para rotarcionamos o boneco para testar
-# A logica, depois serao adicionados os sprites de em pe e rastejando.
-@onready var collisionShape: CollisionShape3D = $CollisionShape3D
-@onready var meshInstance: MeshInstance3D = $MeshInstance3D
-
+var esta_em_obj_escalavel : bool = false
 var movimento_x : float
 var movimento_y : float
 
-# Função de movimentação básica
+func _process(_delta: float) -> void:
+	# Teste temporario para computar dano
+	if Input.is_action_just_pressed("Dano"):
+		computar_dano(dano)
+
 func _physics_process(delta):
+	var vetor_movimentos = calcular_movimento(velocidade_base, 0)
+	
+	movimento_x = vetor_movimentos.x
+	
+	if esta_em_obj_escalavel && not is_on_floor():
+		vetor_movimentos = calcular_movimento(1.0, velocidade_base)
+		movimento_x = vetor_movimentos.x
+		movimento_y = vetor_movimentos.y
+		
+		if vetor_movimentos.y == 0:
+			movimento_y -= 1
+			
+		if Input.is_action_pressed("Pular"):
+			movimento_y = forca_pulo
+	else:
+		if not is_on_floor():
+			movimento_y += gravidade * delta
+		else:
+			# Zera a queda para evitar acúmulo
+			movimento_y = 0
+			# Apenas quando tem contato com o chão
+			if Input.is_action_pressed("Pular"):
+				movimento_y = forca_pulo
+
+	# Chama o método para mover, presente na classe Personagem
+	movimentacao(movimento_x, movimento_y)
+		
+func calcular_movimento(velocidade_x, velocidade_y) -> Vector3:
 	var direcao = Vector3.ZERO
 	
 	# Movimentacao esquerda direita cima baixo
@@ -22,39 +50,20 @@ func _physics_process(delta):
 	if Input.is_action_pressed("Esquerda"):
 		direcao.x -= 1
 	if Input.is_action_pressed("Cima"):
-		direcao.y -= 1
-	if Input.is_action_pressed("Baixo"):
 		direcao.y += 1
+	if Input.is_action_pressed("Baixo"):
+		direcao.y -= 1
 		
 	direcao = direcao.normalized()
 	
-	# Rastejar / andar devagar
-	# Provisorio o que ele muda quando rasteja
-	# No caso as rotacoes.
-	if Input.is_action_pressed("Rastejar"):
-		collisionShape.rotation_degrees.x = 90
-		meshInstance.rotation_degrees.x = 90
-		movimento_x = direcao.x * (velocidade - 2.5)
-	elif Input.is_action_pressed("Devagar"):
-		collisionShape.rotation_degrees.x = 0
-		meshInstance.rotation_degrees.x = 0
-		movimento_x = direcao.x * (velocidade - 2.0)
-	else:
-		collisionShape.rotation_degrees.x = 0
-		meshInstance.rotation_degrees.x = 0
-		movimento_x = direcao.x * velocidade
+	if Input.is_action_pressed("Devagar"):
+		velocidade_x *= 0.4 # 40% da velocidade reduzida
+		
+	var movimento_no_x = direcao.x * velocidade_x
+	var movimento_no_y = direcao.y * velocidade_y
+		
+	return Vector3(movimento_no_x, movimento_no_y, 0)
 	
-	# Pular
-	if not is_on_floor():
-		movimento_y += gravidade * delta
-	else: 
-		movimento_y = 0
-		if Input.is_action_pressed("Pular"):
-			movimento_y = forca_pulo
+func setar_esta_em_escalavel(esta_tocando_escalavel : bool) -> void:
+	esta_em_obj_escalavel = esta_tocando_escalavel
 	
-	
-	movimentacao(movimento_x, movimento_y)
-	
-	# Teste temporario para computar dano
-	if Input.is_action_just_pressed("Dano"):
-		computar_dano(dano)
