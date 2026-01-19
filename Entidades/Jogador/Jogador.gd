878aed3dfb46f264da1676ceadc189b1b8b5ce43
pedrohgdp <pedro.henrique.gp@outlook.com>
2026-01-19 19:08:25 -0300

    FIX/FEAT: Criado sistema de som, com som normal e quando esta lento, e mini refatorada no codigo do jogador ( precisa de mais )

--- Entidades/Jogador/Jogador.gd
+++ Entidades/Jogador/Jogador.gd
@@ -1,6 +1,8 @@
 class_name Jogador
 extends Entidade
 
+enum estados_jogador{ ANDANDO, RASTEJANDO, PARADO, PULANDO, DEVAGAR }
+
 @onready var camera: Camera3D = $pivo_Camera/Camera
 @onready var coracoes_vida: Control = $"../CanvasLayer/BarraVida"
 
@@ -9,10 +11,12 @@ extends Entidade
 @onready var collision_shape : CollisionShape3D = $CollisionShape3D
 @onready var mesh_instance : MeshInstance3D = $MeshInstance3D
 
+var som_passos: AudioStreamPlayer
 var luz_natural_personagem: OmniLight3D
 var esta_em_obj_escalavel : bool = false
 var movimento_x : float
 var movimento_y : float
+var estado_atual = estados_jogador.PARADO
 
 func _ready() -> void:
 	luz_natural_personagem = OmniLight3D.new()
@@ -20,10 +24,16 @@ func _ready() -> void:
 	luz_natural_personagem.omni_range = 0.8
 	get_parent().call_deferred("add_child", luz_natural_personagem)
 	
+	som_passos = AudioStreamPlayer.new()
+	add_child(som_passos)
+	som_passos.stream = load("res://Sons/SFX/Jogador/Passos ( pedra ).wav")
+	som_passos.volume_db = -5
+	
 func _process(_delta: float) -> void:
 	# Teste temporario para computar dano
 	if Input.is_action_just_pressed("Dano"):
 		computar_dano(dano)
+	tocar_som_passos()
 
 func _physics_process(delta):
 	var esta_rastejando = Input.is_action_pressed("Rastejar")
@@ -44,6 +54,7 @@ func _physics_process(delta):
 	else:
 		if not is_on_floor():
 			movimento_y += gravidade * delta
+			estado_atual = estados_jogador.PULANDO
 		else:
 			# Zera a queda para evitar acúmulo
 			movimento_y = 0
@@ -59,21 +70,37 @@ func _physics_process(delta):
 
 func setar_esta_em_escalavel(esta_tocando_escalavel : bool) -> void:
 	esta_em_obj_escalavel = esta_tocando_escalavel
-	
+
+func tocar_som_passos() -> void:
+	if estado_atual == estados_jogador.ANDANDO and is_on_floor():
+		som_passos.pitch_scale = 1.0
+		if not som_passos.playing:
+			som_passos.play()
+	elif estado_atual == estados_jogador.DEVAGAR:
+		som_passos.pitch_scale = 0.4
+		if not som_passos.playing:
+			som_passos.play()
+	else:
+		som_passos.stop()
+
 func calcular_movimento(velocidade_x, velocidade_y) -> Vector3:
-	var direcao = Vector3.ZERO
-	
-	# Movimentação do corpo pelo cenário
-	if Input.is_action_pressed("Direita"):
-		direcao.x += 1
-	if Input.is_action_pressed("Esquerda"):
-		direcao.x -= 1
-	if Input.is_action_pressed("Cima"):
-		direcao.y += 1
-	if Input.is_action_pressed("Baixo"):
-		direcao.y -= 1
+
+	var input_dir = Input.get_vector("Esquerda", "Direita", "Cima", "Baixo")
 		
-	direcao = direcao.normalized()
+	input_dir = input_dir.normalized()
+
+
+	if not is_on_floor():
+		estado_atual = estados_jogador.PULANDO
+	elif Input.is_action_pressed("Rastejar"):
+		estado_atual = estados_jogador.RASTEJANDO
+	elif input_dir.length() > 0:
+		if Input.is_action_pressed("Devagar"):
+			estado_atual = estados_jogador.DEVAGAR
+		else:
+			estado_atual = estados_jogador.ANDANDO
+	else:
+		estado_atual = estados_jogador.PARADO
 	
 	if Input.is_action_pressed("Devagar"):
 		velocidade_x *= 0.4 # 40% da velocidade reduzida
@@ -87,7 +114,7 @@ func calcular_movimento(velocidade_x, velocidade_y) -> Vector3:
 		collision_shape.rotation_degrees.x = 0
 		mesh_instance.rotation_degrees.x = 0
 		
-	var movimento_no_x = direcao.x * velocidade_x
-	var movimento_no_y = direcao.y * velocidade_y
+	var movimento_no_x = input_dir.x * velocidade_x
+	var movimento_no_y = input_dir.y * velocidade_y
 		
 	return Vector3(movimento_no_x, movimento_no_y, 0)
