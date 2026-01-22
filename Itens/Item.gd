7f9172c06bd4cb42d1ef91ab1da2f80c529dc4e7
Wesley <wesleyheringer24@gmail.com>
2026-01-22 12:25:35 -0300

    Adicionando script de item e itensInterragiveis e tambem adicionando sprite ao mapa

---
+++ Itens/Item.gd
@@ -0,0 +1,6 @@
+class_name Item
+extends Resource
+
+@export var nome: String
+@export var tipo: String
+@export var descricao: String
--- Itens/ItensDeInteração/ItensDeInteracao.gd
+++ Itens/ItensDeInteração/ItensDeInteracao.gd
@@ -1,7 +1,7 @@
-class_name Itens
-extends Node
+class_name ItensDeInteracao
+extends Item
 
-
+@export var item : Item
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
