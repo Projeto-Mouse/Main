33c0321d11ff74b474682d22963efa317122ba11
Wesley Heringer de Brito <Wesleyheringer24@gmail.com>
2026-01-25 11:27:08 -0300

    Adiciona scrits e modificações na estrutura de itens, implementado também a lógica de reconhecimento de area do item no jogador e foram adicionado durabilidade ao script de ferramentas

--- Itens/Ferramentas/Tocha.gd
+++ Itens/Ferramentas/Tocha.gd
@@ -1,5 +1,5 @@
-class_name Ferramentas
-extends ItensDeInteracao
+class_name Tocha
+extends Ferramentas
 
 
 # Called when the node enters the scene tree for the first time.
--- Itens/Item.gd
+++ Itens/Item.gd
@@ -1,20 +1,27 @@
 class_name Item
-extends Area3D
+extends Node
+
+enum raridades{COMUM, RARO, MUITO_RARO}
 
 @export var nome: String
 @export var tipo: String
 @export var descricao: String
-
-var jogador_na_area := false
+@export var raridade: raridades
 
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
 
-func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("player"):
-		jogador_na_area = true
+    $Area3D.body_entered.connect(entrou_area_interacao)
+    $Area3D.body_exited.connect(saiu_area_interacao)
 
-func _on_body_exited(body: Node) -> void:
-	if body.is_in_group("player"):
-		jogador_na_area = false
+func entrou_area_interacao(body):
+    print("DEBUG... entrou na area do item!")
+    if body.is_in_group("Jogador"):
+        body.entrou_em_interacao(self)
+
+func saiu_area_interacao(body):
+    print("DEBUG... saiu na area do item!")
+    if body.is_in_group("Jogador"):
+        body.saiu_interacao(self)
+
+func interagir():
+    pass
