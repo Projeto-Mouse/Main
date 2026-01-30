923cb45e03e88bd8f0faff3122e60e827f06d9e3
Matheus Araujo Pinheiro <165668357+Mapnbp@users.noreply.github.com>
2026-01-29 22:31:18 -0300

    feat: detecçao por som PRA CIMA DELES (#125)

--- Cenas/Prologo/Prologo.gd
+++ Cenas/Prologo/Prologo.gd
@@ -10,20 +10,22 @@ func _ready() -> void:
 
 func spawn_inimigos_teste() -> void:
 	var scale_inimigo = 0.15
-	var voador = InimigoVoador.new()
+	var voador: InimigoVoador = InimigoVoador.new()
 	voador.name = "InimigoVoadorTeste"
 	voador.position = Vector3(4, 3, 0)
 	voador.velocidade_base = 2.0
 	setup_inimigo_visual(voador, Color.RED, scale_inimigo)
+	adicionar_sensor_auditivo(voador) # Sistema de detecção de som
 	add_child(voador)
 	
 
-	var terrestre = InimigoTerrestre.new()
+	var terrestre: InimigoTerrestre = InimigoTerrestre.new()
 	terrestre.name = "InimigoTerrestreTeste"
 	terrestre.position = Vector3(6, 1.5, 0)
 	terrestre.velocidade_base = 2.0
 	terrestre.gravidade = 9.8
 	setup_inimigo_visual(terrestre, Color.BLUE, scale_inimigo)
+	adicionar_sensor_auditivo(terrestre)
 	add_child(terrestre)
 
 func spawn_aliado_teste() -> void:
@@ -60,6 +62,18 @@ func setup_inimigo_visual(inimigo: CharacterBody3D, cor: Color, escala: float) -
 	
 	if inimigo.has_method("_on_body_entered"):
 		area.body_entered.connect(inimigo._on_body_entered)
+
+func adicionar_sensor_auditivo(inimigo: CharacterBody3D) -> void:
+	var sensor_script: Script = load("res://Componentes/SensorAuditivo.gd")
+	
+	var sensor = Node3D.new()
+	sensor.name = "SensorAuditivo"
+	sensor.set_script(sensor_script)
+	
+	sensor.set("alcance_maximo", 10.0)
+	sensor.set("mascara_oclusao", 1)
+	
+	inimigo.add_child(sensor)
 
 # Essa funcao da godot eh chamada em todos os frames. Atenção ao uso da mesma, pode pesar o código
 func _process(_delta: float) -> void:
