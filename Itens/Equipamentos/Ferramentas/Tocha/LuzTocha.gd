3a1cbbd8d826a9037af456c9b5b7ec404634f507
pedrohgdp <pedro.henrique.gp@outlook.com>
2026-02-04 19:06:34 -0500

    FIX: MUDANCAS PROPOSTAS CORRIGIDAS, SE TIVEREM ALGUMA IDEIA BOA MATEMATICA PARA MIN, MAX E FATOR_ITENCIDADE PARA DEIXAR MAIS NATURAL ACEITO

--- Itens/Equipamentos/Ferramentas/Tocha/LuzTocha.gd
+++ Itens/Equipamentos/Ferramentas/Tocha/LuzTocha.gd
@@ -1,23 +1,26 @@
+class_name LuzTocha
 extends OmniLight3D
 
-var geradorNoise: FastNoiseLite = FastNoiseLite.new()
-var energiaBase: float
+var gerador_noise: FastNoiseLite = FastNoiseLite.new()
+var energia_base: float
+var fator_intensidade = 0.5
 
 var tempo: float = 0.0
 
 func _ready() -> void:
-    geradorNoise.noise_type = FastNoiseLite.TYPE_PERLIN
-    geradorNoise.frequency = 1.0
-    geradorNoise.seed = 1547
-    energiaBase = light_energy
+    gerador_noise.noise_type = FastNoiseLite.TYPE_PERLIN
+    gerador_noise.frequency = 1.0
+    gerador_noise.seed = 1547
+    energia_base = light_energy
 
 func _process(delta: float) -> void:
     tempo += delta
     flicarLuz()
 
 func flicarLuz() -> void:
-    var noiseLento = geradorNoise.get_noise_1d(tempo * 0.35)
-    var noiseRapido = geradorNoise.get_noise_1d(tempo * 3.0)
+    var noise_lento = gerador_noise.get_noise_1d(tempo * 0.35)
+    var noise_rapido = gerador_noise.get_noise_1d(tempo * 2.0)
 
-    light_energy = clamp((energiaBase + noiseLento + noiseRapido), 0.50, 0.95)
+    print("Energia luz: ", light_energy)
+    light_energy = clamp(energia_base + ((noise_lento + noise_rapido) * fator_intensidade), 0.3, 1.2)
     
