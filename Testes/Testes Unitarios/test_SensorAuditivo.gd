25a4a5c5f664ef624398dd1b246881f29af25469
pedrohgdp <pedro.henrique.gp@outlook.com>
2026-02-08 19:00:15 -0500

    FEAT: criado teste para controlador iluminacao, e algumas mudancas para deixar codigo mais padrao, alem de tirar comentarios redundantes

--- Testes/Testes Unitarios/test_SensorAuditivo.gd
+++ Testes/Testes Unitarios/test_SensorAuditivo.gd
@@ -1,11 +1,10 @@
 extends GutTest
 
-var sensor_auditivo = load("res://Componentes/SensorAuditivo.gd") 
-
+var script_sensor_auditivo = load("res://Componentes/SensorAuditivo.gd") 
 var sensor
 
 func before_each(): 
-	sensor = partial_double(sensor_auditivo).new() 
+	sensor = partial_double(script_sensor_auditivo).new() 
 	add_child_autofree(sensor)
 
 func test_ao_ruido_percebido_oclusao_true() -> void: 
@@ -14,7 +13,6 @@ func test_ao_ruido_percebido_oclusao_true() -> void:
 	sensor.global_position = Vector3.ZERO
 	var retorno_funcao = sensor._ao_ruido_percebido(Vector3(1.0, 0, 0), 5.0)
 	
-	# Verifica se foi retornado false
 	assert_false(retorno_funcao)
 
 
@@ -24,7 +22,6 @@ func test_ao_ruido_percebido_oclusao_false() -> void:
 	sensor.global_position = Vector3.ZERO
 	var retorno_funcao = sensor._ao_ruido_percebido(Vector3(1.0, 0, 0), 5.0)
 	
-	# Verifica se da true, por que passou
 	assert_true(retorno_funcao)
 	
 
@@ -32,6 +29,5 @@ func test_ao_ruido_percebido_distancia_maior_alcance() -> void:
 	sensor.global_position = Vector3.ZERO
 	var retorno_funcao = sensor._ao_ruido_percebido(Vector3(1000.0, 1000.0, 1000.0), 5.0)
 	
-	# Verifica se retorna false
 	assert_false(retorno_funcao)
 	
