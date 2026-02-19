acc6e3af9904c252154f3b245141d2374eef66bd
pedrohgdp <pedro.henrique.gp@outlook.com>
2026-02-19 11:42:56 -0300

    Teste pré commit

--- Entidades/Entidade.gd
+++ Entidades/Entidade.gd
@@ -1,7 +1,7 @@
 class_name Entidade
 extends CharacterBody3D
 
-# Variaveis fisicas 
+# Variaveis fisicas
 @export var velocidade_base: float
 @export var gravidade: float
 @export var forca_pulo: float
@@ -12,32 +12,40 @@ extends CharacterBody3D
 @export var vida_atual: float
 @export var dano: float
 
+
 func _ready() -> void:
 	pass
+
 
 func movimentacao(movimento_x: float, movimento_y: float):
 	# Velocidade para eixo z zerada nao usamos
 	velocity.z = 0
 	velocity.x = movimento_x
 	velocity.y = movimento_y
-	
+
 	move_and_slide()
+
 
 func criar_raycast() -> RayCast3D:
 	var raycast_visao: RayCast3D
 	raycast_visao = RayCast3D.new()
 	return raycast_visao
 
-func configurar_raycast(raycast_visao: RayCast3D, ray_ativo: bool, mascara: int, ignorar_pai: bool) -> void:
+
+func configurar_raycast(
+	raycast_visao: RayCast3D, ray_ativo: bool, mascara: int, ignorar_pai: bool
+) -> void:
 	raycast_visao.enabled = ray_ativo
 	raycast_visao.collision_mask = mascara
 	raycast_visao.exclude_parent = ignorar_pai
-	
+
+
 func computar_dano(dano_recebido: float) -> void:
 	vida_atual -= dano_recebido
 	if vida_atual <= 0:
 		vida_atual = 0
 		print("Personagem Morreu")
+
 
 func vetor_para(entidade: Entidade) -> Vector3:
 	return entidade.global_position - global_position
