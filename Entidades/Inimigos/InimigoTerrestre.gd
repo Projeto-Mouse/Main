c981c6f68df192ed40a07d4c93d46ff847a6d89a
Pedro Henrique Gonçalves de Paula <69373766+pedrohgdp@users.noreply.github.com>
2026-03-09 16:00:47 -0300

    Pmdt 131 132 133 134 implementar abstract classes npcs em geral (#194)
    * FEAT: Adicionado abstract nas classes que devem ser abstratas e alguns metodos ( trabalhado apenas em inimigos, aliados, jogador, entidade )
    
    * FIX: Pequena mudanca para rodar o formatter automatico agora no pc
    
    * FIX: Pequena mudanca para rodar o formatter automatico agora no pc
    
    * FIX: DESCULPA, DEU UM ERRO EU ESTOU CONFIGURANDO SO AGORA VAI CERTO
    
    * Removendo comentarios com caractere especial

--- Entidades/Inimigos/InimigoTerrestre.gd
+++ Entidades/Inimigos/InimigoTerrestre.gd
@@ -4,10 +4,11 @@ extends Inimigo
 var tempo_mudanca_direcao = 0.0
 var move_dir = 0.0
 
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravidade * delta
-		
+
 	atualizar_raycast_direcao_movimento()
 	target()
 	atualizar_linha_debug()
@@ -15,13 +16,15 @@ func _physics_process(delta: float) -> void:
 	verificar_dano_contato()
 
 
-func movimento_simples() -> void:
+func movimentacao() -> void:
 	velocity.x = move_dir * velocidade_base
+	move_and_slide()
+
 
 func gerar_movimento_aleatorio() -> void:
 	var delta = get_physics_process_delta_time()
 	tempo_mudanca_direcao -= delta
-	
+
 	if tempo_mudanca_direcao <= 0:
 		tempo_mudanca_direcao = randf_range(1.0, 4.0)
 		# Escolhe: esquerda, direita ou parado
@@ -32,7 +35,9 @@ func gerar_movimento_aleatorio() -> void:
 			move_dir = 1.0
 		else:
 			move_dir = 0.0
-	
-	movimento_simples()
-	
-	move_and_slide()
+
+	movimentacao()
+
+
+func computar_dano(dano_recebido: float) -> void:
+	pass
