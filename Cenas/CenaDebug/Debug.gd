ab4c2529040e823c748f70936547a31dbaec5ecc
Pedro Henrique Gonçalves de Paula <69373766+pedrohgdp@users.noreply.github.com>
2026-03-10 13:28:44 -0300

    Pmdt 104 modularizar setup iluminacao (#205)
    * FEAT: Adicionado painel para botoes de debug
    
    * FEAT: Adicionado botao para trocar  dia e noite e slide ( nao configurado ) para mudar tempo
    
    * FEAT: Adicionado slide funcional no debug de iluminacao
    
    * FIX: Tirando comentarios escrotos

--- Cenas/CenaDebug/Debug.gd
+++ Cenas/CenaDebug/Debug.gd
@@ -2,11 +2,18 @@ class_name Debug
 extends Node3D
 
 @onready var menu_de_pause: Control = $Jogador/pivo_Camera/Camera/MenuDePausa
+@onready var controlador_menu_debug = $MenuDebug/MenuDebug
+var controlador_iluminacao = Node3D.new()
 var nao_pausado = false
 
 
 func _ready() -> void:
 	setup_iluminacao()
+
+# Essa funcao da godot eh chamada em todos os frames. Atenção ao uso da mesma, pode pesar o código
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Pausar"):
+		mostrar_menu_de_pausa()
 
 
 func setup_iluminacao() -> void:
@@ -15,20 +22,19 @@ func setup_iluminacao() -> void:
 		push_error("Script ControladorIluminacao.gd não encontrado!")
 		return
 
-	var controlador = Node3D.new()
-	controlador.name = "ControladorIluminacao"
-	controlador.set_script(ScriptIluminacao)
+	controlador_iluminacao.name = "ControladorIluminacao"
+	controlador_iluminacao.set_script(ScriptIluminacao)
 	# add_child movido para o final da função setup_iluminacao para evitar warnings no _ready
 
 	var camera = $Jogador/pivo_Camera/Camera
 	if camera:
-		controlador.camera_alvo = camera
+		controlador_iluminacao.camera_alvo = camera
 	else:
 		push_warning("Camera não encontrada em Jogador/pivo_Camera/Camera")
 
 	var sol = get_node_or_null("DirectionalLight3D")
 	if sol:
-		controlador.luz_direcional = sol
+		controlador_iluminacao.luz_direcional = sol
 	else:
 		push_warning("DirectionalLight3D não encontrada na raiz da cena")
 
@@ -38,11 +44,11 @@ func setup_iluminacao() -> void:
 	spot.spot_range = 20.0
 	spot.spot_angle = 45.0
 	add_child(spot)
-	controlador.luz_spot = spot
+	controlador_iluminacao.luz_spot = spot
 
 	var env = get_node_or_null("WorldEnvironment")
 	if env:
-		controlador.ambiente_mundial = env
+		controlador_iluminacao.ambiente_mundial = env
 	else:
 		var novo_env = WorldEnvironment.new()
 		novo_env.name = "WorldEnvironment"
@@ -55,15 +61,10 @@ func setup_iluminacao() -> void:
 		novo_env.environment = environment
 		add_child(novo_env)
 
-		controlador.ambiente_mundial = novo_env
+		controlador_iluminacao.ambiente_mundial = novo_env
 		print("WorldEnvironment criado automaticamente.")
 
-	add_child(controlador)
-
-# Essa funcao da godot eh chamada em todos os frames. Atenção ao uso da mesma, pode pesar o código
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("Pausar"):
-		mostrar_menu_de_pausa()
+	add_child(controlador_iluminacao)
 
 func mostrar_menu_de_pausa():
 	if nao_pausado:
