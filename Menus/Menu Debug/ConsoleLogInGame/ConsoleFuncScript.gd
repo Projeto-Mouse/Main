53284ca38f5eea78a1f209d7e6a388ffc449433a
pedrohgdp <pedro.henrique.gp@outlook.com>
2026-03-13 09:47:02 -0300

    FEAT: Criado scripts para console e para controlar RichTextLabel, criado o console graficamente

---
+++ Menus/Menu Debug/ConsoleLogInGame/ConsoleFuncScript.gd
@@ -0,0 +1,9 @@
+extends Node
+
+# Esse script trata de todas as funcoes pro log
+# Funcoes como adicionar um texto
+# limpar o log
+# Adicionar um texto com cor
+# Como ele vai ser so um script usado em algo, nao vai ser criado no mundo
+# Creio que nao precisamos botar class_name nele
+# Me corrigam se eu estiver errado
