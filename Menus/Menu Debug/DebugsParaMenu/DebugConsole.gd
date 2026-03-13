efd92930765a017e002eef84a8e9cdd1717f8cab
pedrohgdp <pedro.henrique.gp@outlook.com>
2026-03-13 10:31:12 -0300

    FEAT: Criado botao de ir para o final

--- Menus/Menu Debug/DebugsParaMenu/DebugConsole.gd
+++ Menus/Menu Debug/DebugsParaMenu/DebugConsole.gd
@@ -1,11 +1,6 @@
+class_name DebugConsole
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@onready var limpar_button = $LimparButton
+@onready var terminal = $Terminal
+@onready var ir_para_final_button = $IrParaFinalButton
