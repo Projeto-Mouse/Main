6e6c8eff92f5f52535f05fa89abe8d915e02814f
pedrohgdp <pedro.henrique.gp@outlook.com>
2026-03-16 16:59:14 -0300

    FEAT: ADICIONADO MODO GOD

---
+++ Menus/Menu Debug/DebugsParaMenu/DebugGodMode.gd
@@ -0,0 +1,24 @@
+class_name DebugGodMode
+extends Button
+
+var jogador
+
+func _ready() -> void:
+	jogador = get_tree().get_first_node_in_group("Jogador")
+
+	focus_mode = Control.FOCUS_NONE
+	
+	pressed.connect(ativar_modo_god)
+	
+
+func ativar_modo_god() -> void:
+	if not jogador:
+		DebugConsole.add_text_console_com_cor("Jogador não encontrado!", Color.YELLOW)
+		return
+		
+	jogador.modo_god = !jogador.modo_god
+	
+	if jogador.modo_god:
+		DebugConsole.add_text_console_sem_cor("Modo god ativado")
+	else:
+		DebugConsole.add_text_console_sem_cor("Modo god desativado")
