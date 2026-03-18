498074c619c4981ea7f5fbbb4ddc50e19331eb19
Pedro Henrique Gonçalves de Paula <69373766+pedrohgdp@users.noreply.github.com>
2026-03-18 18:53:55 -0300

    Criando Console in-game (#209)
    * FEAT: Criado scripts para console e para controlar RichTextLabel, criado o console graficamente
    
    * FEAT: Criado botao de ir para o final
    
    * FEAT: Criado funcoes necessarias no arquivo de script que controla o que é feito no label
    
    * FEAT/FIX: Concertado print de estados e implementado funcoes deimprimir texto no console de debug. Alem de trocado alguns nomes
    
    * FIX: Tirando jogadordebug e voltando jogador ao normal para aplicarmos tudo no mesmo script sem separar em jogadordebug e normal
    
    * FEAT: Adicionado a funcao dos botoes de ir para o final e limpar o console
    
    * Criar Spawn Aliado (#210)
    
    * FEAT: Criado spawn de alidado no click do mouse no arquivo debugspawn
    
    * FIX: Melhorado o codigo de spawn
    
    * FIX: Corrigido raycast
    
    * FIX: VOLTADO PARA RETORNAR VECTOR 3 ZERO
    
    * Implementado Modo God ( E outros ) (#211)
    
    * FEAT: Adicionando fps e quantidade de entidades spawnadas
    
    * FEAT: ADICIONADO MODO GOD
    
    * FIX: Corrigindo que o canvaslayer bloqueava o uso do menu ao pausar, fecha-se o menu quando aperta esc agora
    
    * FIX: Desativando o modo dev ao sair pelo botao sair
    
    * Criando ferramenta tp (#212)
    
    * FEAT: Adicionado sistema de dar tp, apertando no botao e clicando NO CHAO para onde quer ir
    
    * FEAT: Adicionado sistema de dar tp, apertando no botao e clicando NO CHAO para onde quer ir
    
    * FIX: Ajeitando para instanciarmos os inimigos e alidos como filhos da cena 3d nao control, adicionando offset no y para nao ficar na parede, adicionando parede para teste
    
    * FIX: Melhorando a funcao de normalizar o click do mouse
    
    * Fix god mode toggle message display
    
    * ADICIONANDO COMENTARIO POR ENQUANTO E DEIXANDO ELSE
    
    * FIX: Adicionando eazy return

--- Menus/Menu Debug/Debugs/DebugSpawn.gd
+++ Menus/Menu Debug/Debugs/DebugSpawn.gd
@@ -3,77 +3,91 @@ extends Control
 
 const SCALE_INIMIGO = 0.15
 
+enum tipo_spawn { NENHUM, VOADOR, TERRESTRE, ALIADO }
+
 @onready var botao_spawnar_terrestre = $Terrestre
 @onready var botao_spawnar_voador = $Voador
+@onready var botao_spawnar_aliado = $Aliado
+@onready var quantidade_entidades_texto = $QtdEntidades
 
 var posicao_spawnar: Vector3
-var click_spawnar_terrestre: bool = false
-var click_spawnar_voador: bool = false
+var spawn_atual = tipo_spawn.NENHUM
+var quantidade_entidades = 0
+
+var cena_debug: Node3D
 
 
 func _ready() -> void:
+	cena_debug = get_tree().get_first_node_in_group("debug")
 	posicao_spawnar = Vector3.ZERO
 
 	botao_spawnar_terrestre.focus_mode = Control.FOCUS_NONE
 	botao_spawnar_voador.focus_mode = Control.FOCUS_NONE
+	botao_spawnar_aliado.focus_mode = Control.FOCUS_NONE
 
 	botao_spawnar_terrestre.pressed.connect(_ao_apertar_botao_spawnar_terrestre)
 	botao_spawnar_voador.pressed.connect(_ao_apertar_botao_spawnar_voador)
+	botao_spawnar_aliado.pressed.connect(_ao_apertar_botao_spawnar_alidao)
 
 
-func _process(delta: float) -> void:
-	if click_spawnar_terrestre and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		spawnar_terrestre_na_pos_click()
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		match spawn_atual:
+			tipo_spawn.TERRESTRE:
+				spawnar_inimigo_terrestre(get_viewport().get_mouse_position())
+				quantidade_entidades += 1
+			tipo_spawn.VOADOR:
+				spawnar_inimigo_voador(get_viewport().get_mouse_position())
+				quantidade_entidades += 1
+			tipo_spawn.ALIADO:
+				spawnar_aliado(get_viewport().get_mouse_position())
+				quantidade_entidades += 1
 
-	if click_spawnar_voador and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		spawnar_voador_na_pos_click()
-
-
-func spawnar_terrestre_na_pos_click() -> void:
-	var posicao_click = get_viewport().get_mouse_position()
-	click_spawnar_terrestre = false
-	spawnar_inimigo_terrestre(posicao_click)
-
-
-func spawnar_voador_na_pos_click() -> void:
-	var posicao_click = get_viewport().get_mouse_position()
-	click_spawnar_voador = false
-	spawnar_inimigo_voador(posicao_click)
+		quantidade_entidades_texto.text = "Entidades spawnadas: " + str(quantidade_entidades)
+		spawn_atual = tipo_spawn.NENHUM
 
 
 func _ao_apertar_botao_spawnar_terrestre() -> void:
-	click_spawnar_terrestre = true
+	spawn_atual = tipo_spawn.TERRESTRE
 
 
 func _ao_apertar_botao_spawnar_voador() -> void:
-	click_spawnar_voador = true
+	spawn_atual = tipo_spawn.VOADOR
+
+
+func _ao_apertar_botao_spawnar_alidao() -> void:
+	spawn_atual = tipo_spawn.ALIADO
 
 
 func spawnar_inimigo_terrestre(posicao_click: Vector2) -> void:
 	print("Spawnar terrestre chamado")
+	DebugConsole.add_text_console_sem_cor("Spawnar terrestre chamado")
 	var terrestre: Inimigo = InimigoTerrestre.new()
 	posicao_spawnar = normalizar_pos_3d(posicao_click)
 	posicao_spawnar.z = 0.1
+	posicao_spawnar.y += 1.0
 	terrestre.name = "InimigoTerrestreTeste"
 	terrestre.position = posicao_spawnar
 	terrestre.velocidade_base = 2.0
 	terrestre.gravidade = 9.8
 	setup_inimigo_visual(terrestre, Color.BLUE, SCALE_INIMIGO)
 	adicionar_sensor_auditivo(terrestre)
-	add_child(terrestre)
+	cena_debug.add_child(terrestre)
 
 
 func spawnar_inimigo_voador(posicao_click: Vector2) -> void:
 	print("Spawnar voador chamado")
+	DebugConsole.add_text_console_sem_cor("Spawnar voador chamado")
 	var voador: Inimigo = InimigoVoador.new()
 	posicao_spawnar = normalizar_pos_3d(posicao_click)
 	posicao_spawnar.z = 0.1
+	posicao_spawnar.y += 1.0
 	voador.name = "InimigoVoadorTeste"
 	voador.position = posicao_spawnar
 	voador.velocidade_base = 2.0
 	setup_inimigo_visual(voador, Color.RED, SCALE_INIMIGO)
 	adicionar_sensor_auditivo(voador)  # Sistema deteccao de som
-	add_child(voador)
+	cena_debug.add_child(voador)
 
 
 func setup_inimigo_visual(inimigo: CharacterBody3D, cor: Color, escala: float) -> void:
@@ -117,24 +131,34 @@ func adicionar_sensor_auditivo(inimigo: CharacterBody3D) -> void:
 	inimigo.add_child(sensor)
 
 
+func spawnar_aliado(posicao_click: Vector2) -> void:
+	DebugConsole.add_text_console_sem_cor("Aliado Adicionado")
+	var aliado: Aliados = AliadoInterativo.new()
+	posicao_spawnar = normalizar_pos_3d(posicao_click)
+	posicao_spawnar.z = 0.0
+	posicao_spawnar.y += 1.0
+	aliado.name = "AliadoInterativoTeste"
+	aliado.lealdade = 10.0
+	aliado.position = posicao_spawnar
+	aliado.velocidade_base = 2.0
+	setup_inimigo_visual(aliado, Color.GREEN, SCALE_INIMIGO)
+	cena_debug.add_child(aliado)
+
+
 func normalizar_pos_3d(pos_click: Vector2) -> Vector3:
 	var camera = get_viewport().get_camera_3d()
-
 	if !camera:
 		print("nao pegou camera")
+		DebugConsole.add_text_console_sem_cor("nao pegou camera")
 		return Vector3.ZERO
 
-	var raio = 500
-
 	var origem = camera.project_ray_origin(pos_click)
-	var destino = (camera.project_ray_normal(pos_click) * raio) + origem
+	var direcao = camera.project_ray_normal(pos_click)
 
-	var estado_espaco = camera.get_world_3d().direct_space_state
-	var query = PhysicsRayQueryParameters3D.create(origem, destino)
+	var plano_z_zero = Plane(Vector3(0, 0, 1), 0.0)
 
-	var resultado = estado_espaco.intersect_ray(query)
+	var posicao_intersecao = plano_z_zero.intersects_ray(origem, direcao)
 
-	if resultado:
-		return resultado.position
-
+	if posicao_intersecao != null:
+		return posicao_intersecao
 	return Vector3.ZERO
