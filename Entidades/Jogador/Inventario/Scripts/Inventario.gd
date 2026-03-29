172b58d7298abbf0c39743aadd2717f3a1cf910b
pedrohgdp <pedro.henrique.gp@outlook.com>
2026-03-29 16:04:38 -0300

    FEAT: Fim da funcao de adicionar item

--- Entidades/Jogador/Inventario/Scripts/Inventario.gd
+++ Entidades/Jogador/Inventario/Scripts/Inventario.gd
@@ -9,6 +9,8 @@ var array_inventario: Array[Slot] = []
 
 var mapa_itens: Dictionary = {}
 
+var slots_vazios: Array[int] = []
+
 
 func _ready() -> void:
 	inicializar_inventario()
@@ -18,6 +20,7 @@ func inicializar_inventario() -> void:
 	array_inventario.resize(tamanho)
 	for i in range(tamanho):
 		array_inventario[i] = Slot.new()
+		slots_vazios.append(i)
 
 	DebugConsole.add_text_console_sem_cor("Inventario inicializado com tamanho" + str(tamanho))
 	print("Inventario inicializado com tamanho" + str(tamanho))
@@ -42,7 +45,7 @@ func adicionar_item_inventario(item: ItemData, quantidade_add: int) -> void:
 	for i in indices:
 		if array_inventario[i].quantidade < MAX_STACK:
 			var espaco_livre = MAX_STACK - array_inventario[i].quantidade
-			var quantidade_a_adicionar = min(array_inventario[i].quantidade, espaco_livre)
+			var quantidade_a_adicionar = min(quantidade_add, espaco_livre)
 
 			array_inventario[i].quantidade += quantidade_a_adicionar
 			quantidade_add -= quantidade_a_adicionar
@@ -50,5 +53,20 @@ func adicionar_item_inventario(item: ItemData, quantidade_add: int) -> void:
 			if quantidade_add == 0:
 				return
 
-		while quantidade_add > 0:
-			pass
+	while quantidade_add > 0:
+		if slots_vazios.is_empty():
+			print("Inventario cheio")
+			DebugConsole.add_text_console_sem_cor("Inventario cheio")
+			return
+
+		var slot_vazio = slots_vazios.pop_back()
+		array_inventario[slot_vazio].item = item
+
+		var resto_para_adicionar = min(quantidade_add, MAX_STACK)
+		array_inventario[slot_vazio].quantidade = resto_para_adicionar
+		quantidade_add -= resto_para_adicionar
+
+		if not mapa_itens.has(item.nome):
+			mapa_itens[item.nome] = []
+
+		mapa_itens[item.nome].append(slot_vazio)
