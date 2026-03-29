15881d106d0f9bcdc53ccdd411bf5105488891e5
pedrohgdp <pedro.henrique.gp@outlook.com>
2026-03-29 13:56:01 -0300

    FEAT: Mudando item data

--- Itens/ItemData.gd
+++ Itens/ItemData.gd
@@ -1,11 +1,11 @@
-@abstract
-class_name ItemData
+@abstract class_name ItemData
 extends Resource
 
-enum raridades{COMUM, RARO, MUITO_RARO}
+enum raridades { COMUM, RARO, MUITO_RARO }
 
 @export var nome: String
 @export var tipo: String
 @export var descricao: String
 @export var raridade: raridades
 @export var cena_3d: PackedScene
+@export var empilhavel: bool
