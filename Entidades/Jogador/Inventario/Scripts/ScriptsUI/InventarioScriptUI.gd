554e9f806a25760c3f045404ff7d9f8921de2335
pedrohgdp <pedro.henrique.gp@outlook.com>
2026-03-30 15:34:33 -0300

    FEAT: Feito sistema de mouse em cima

--- Entidades/Jogador/Inventario/Scripts/ScriptsUI/InventarioScriptUI.gd
+++ Entidades/Jogador/Inventario/Scripts/ScriptsUI/InventarioScriptUI.gd
@@ -1,7 +1,7 @@
 class_name InventarioScriptUI
 extends CanvasLayer
 
-@onready var grid_inventario = $InventarioUIControl/TextureRect/MarginContainer/GridContainer
+@onready var grid_inventario = $InventarioUIControl/TextureRect/GridContainer
 @onready var root_control = $InventarioUIControl
 @onready var textura_inventario = $InventarioUIControl/TextureRect
 
@@ -9,11 +9,12 @@ var quantidade_slots: int
 var array_slots: Array = []
 var inventario: Inventario
 var slot_cena = preload("res://UI/Inventario/SlotUI.tscn")
-var slots_do_grid = grid_inventario.get_children()
-var colunas_do_grid = grid_inventario.get_columns
+var slots_do_grid
+var indice_slot_selecionado
 
 func iniciar_slots() -> void:
 	array_slots.clear()
+	
 	for i in range(quantidade_slots):
 		var slot = slot_cena.instantiate()
 		slot.name = "slot" + str(i)
@@ -22,10 +23,11 @@ func iniciar_slots() -> void:
 		grid_inventario.add_child(slot)
 		print(slot.name, " Criado com sucesso")
 		DebugConsole.add_text_console_com_cor(slot.name + " Criado com sucesso", Color.CORAL)
+	
 	atualizar_tamanho_ui()
 	pegar_irmoes_cada_slot()
-	slots_do_grid[0].grab_focus()
-
+	
+	slots_do_grid[0].call_deferred("grab_focus")
 
 func set_inventario(inv: Inventario):
 	inventario = inv
@@ -39,6 +41,8 @@ func atualizar_tamanho_ui():
 	textura_inventario.size = tamanho
 
 func pegar_irmoes_cada_slot() -> void:
+	slots_do_grid = grid_inventario.get_children()
+	var colunas_do_grid: int = grid_inventario.columns
 	for i in range(slots_do_grid.size()):
 		
 		var baixo = i + colunas_do_grid
@@ -54,8 +58,8 @@ func pegar_irmoes_cada_slot() -> void:
 			slot.focus_neighbor_left = slots_do_grid[i - 1].get_path()
 
 		if  baixo < slots_do_grid.size():
-			slot.focus_neighbor_bottom = slots_do_grid[i + colunas_do_grid].get_path()
+			slot.focus_neighbor_bottom = slots_do_grid[baixo].get_path()
 
 		if cima >= 0:
-			slot.focus_neighbor_top = slots_do_grid[i - colunas_do_grid].get_path()
+			slot.focus_neighbor_top = slots_do_grid[cima].get_path()
 		
--- Entidades/Jogador/Inventario/Scripts/ScriptsUI/SlotScriptUI.gd
+++ Entidades/Jogador/Inventario/Scripts/ScriptsUI/SlotScriptUI.gd
@@ -1,7 +1,26 @@
 class_name SlotScriptUI
 extends Control
 
-var indice_slot: int
+@onready var textura = $Textura
+
+var sprite_selecionado = preload("res://Sprites/Menu Inventario 1/Sprite-inv-mouse-above.png")
+var sprite_normal = preload("res://Sprites/Menu Inventario 1/Sprite-inv-padrao.png")
+var sprite_pressionado = preload("res://Sprites/Menu Inventario 1/Sprite-inv-selected.png")
+var sprite_selecionado_no_mover = preload("res://Sprites/Menu Inventario 1/Sprite-inv-to-move.png")
 
 func _ready() -> void:
 	focus_mode = Control.FOCUS_ALL
+	
+	connect("focus_entered", _entrou_em_foco)
+	connect("focus_exited", _saiu_de_foco)
+	connect("mouse_entered", _entrou_em_foco)
+	connect("mouse_exited", _saiu_de_foco)
+
+func _entrou_em_foco():
+	textura.texture = sprite_selecionado
+
+func _saiu_de_foco():
+	textura.texture = sprite_normal
+
+func _mouse_entrou() -> void:
+	textura.texture = sprite_selecionado
