6742dca134491e3e843598dd477c768e2fb04988
pedrohgdp <pedro.henrique.gp@outlook.com>
2026-04-01 18:44:12 -0300

    FEAT: Criado controller inventario

--- UI/Inventario/ScriptsUI/SlotScriptUI.gd
+++ UI/Inventario/ScriptsUI/SlotScriptUI.gd
@@ -1,6 +1,8 @@
 class_name SlotScriptUI
 extends Control
 
+enum TiposSlot{ HOTBAR, INVENTARIO }
+
 @onready var textura = $Textura
 @onready var item_sprite = $ItemSprite
 @onready var quantidade = $Quantidade
@@ -9,6 +11,11 @@ var sprite_selecionado = preload("res://Sprites/Menu Inventario 1/Sprite-inv-mou
 var sprite_normal = preload("res://Sprites/Menu Inventario 1/Sprite-inv-padrao.png")
 var sprite_pressionado = preload("res://Sprites/Menu Inventario 1/Sprite-inv-selected.png")
 var sprite_selecionado_no_mover = preload("res://Sprites/Menu Inventario 1/Sprite-inv-to-move.png")
+
+var tipo_slot: TiposSlot
+var indice_slot: int
+var esta_selecionado: bool
+var controller_inventario: ControllerInventario
 
 func _ready() -> void:
 	await get_tree().process_frame
@@ -21,13 +28,23 @@ func _ready() -> void:
 	mouse_exited.connect(_mouse_saiu)
 	
 func _entrou_em_foco():
-	textura.texture = sprite_selecionado
+	esta_selecionado = true
+	_atualizar_textura()
 
 func _saiu_de_foco():
-	textura.texture = sprite_normal
+	esta_selecionado = false
+	_atualizar_textura()
 
 func _mouse_entrou() -> void:
-	textura.texture = sprite_selecionado
-
+	esta_selecionado = true
+	_atualizar_textura()
+	
 func _mouse_saiu() -> void:
-	textura.texture = sprite_normal
+	esta_selecionado = false
+	_atualizar_textura()
+	
+func _atualizar_textura() -> void:
+	pass
+
+func atualiar_para_amarelo_quando_ja_tem_clicado() -> void:
+	pass
